70151c7b40777bfd868379ca9d4cb193b2262f6b
lirongliang <1843306093@qq.com>
2020-05-21 16:44:05 +0800

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -3,6 +3,8 @@ module.exports = {
     clientSecret: '84fa2f6a93408c796db8e190467b8c17051140eb', // github的cclientSecret
     msgKey: 'c32cc2c0ea084c9db36d9243041078f1', // 短信发送的key
     emailPass: 'pbzazeuezsewfbfb', // 发送邮件的pass
+     accessKeyId: 'LTAI4FpCzJFeLcvtwkhsVukz', // 阿里云短信key
+    secretAccessKey: 'wrybZ2IhfvsCkMYjl79jeSN0tjZJIp', // 阿里云短信accessKey
     scope: ['user'],
     secret: 'lrlaym',
     whitePath: ['/', '/users/login', '/users/register', '/users/captcha', '/users/phoneLogin', '/users/sendMsg',
