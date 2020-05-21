5630f587db0d341b6ee14144bbc8488e4c5a7745
lirongliang <1843306093@qq.com>
2020-05-21 09:23:47 +0800

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -5,8 +5,6 @@ module.exports = {
     emailPass: 'pbzazeuezsewfbfb', // 发送邮件的pass
     scope: ['user'],
     secret: 'lrlaym',
-    accessKeyId: 'LTAI4FpCzJFeLcvtwkhsVukz', // 阿里云短信key
-    secretAccessKey: 'wrybZ2IhfvsCkMYjl79jeSN0tjZJIp', // 阿里云短信accessKey
     whitePath: ['/', '/users/login', '/users/register', '/users/captcha', '/users/phoneLogin', '/users/sendMsg',
         '/users/findPwd', '/users/logout', , /^\/api/, /\.(jpg|jepg|gif|png)$/
 
